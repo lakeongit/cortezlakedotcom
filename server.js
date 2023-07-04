77bebe6b60f95c38f796befa61d2169986e18825
lakeongit <122133828+lakeongit@users.noreply.github.com>
2023-07-03 23:00:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // server.js
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet()); // for security
+app.use(compression()); // for performance
 app.use(express.static('public'));
+
+app.use((err, req, res, next) => { // error handling middleware
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
